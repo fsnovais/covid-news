3d4f735cff2f415eab4624763ae71f62ed2ce478
Felipe Novais <felipekun213@gmail.com>
2020-05-26 17:07:47 -0300

    1.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Footer from './Footer.js';
 import './Footer.css';
 import logo from './assets/coronaNews.png';
 import news from './assets/news.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
 require('dotenv').config()
+
 
 class App extends Component {
   render() {
@@ -15,10 +18,16 @@ class App extends Component {
         <div className="appHeader">
           <img className="appLogo" src={logo} alt="Covid" />
 
+
           <div className="title">
             <h1>COVID NEWS</h1>
             <h4>
-              <a> Tudo que você precisa saber sobre o coronavirus </a>
+              <a> Tudo que você precisa saber sobre o coronavirus </a> 
+
+              <p><FontAwesomeIcon icon={faGamepad} aria-hidden="true" />
+              &nbsp;Clique <a href="https://github.com/leandronowras/isolate-SO"><b>aqui</b> </a>
+              para baixar um incrivel jogo e se divertir durante o isolamento
+              </p>
             </h4>
           </div>
 
